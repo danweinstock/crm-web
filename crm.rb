f759d157d7eebd975b2183bfaef54d221dbea6b2
danweinstock <dan.weinstock@yahoo.com>
2015-01-29 15:35:10 -0500

    Jan29 initial commit

--- crm.rb
+++ crm.rb
@@ -4,7 +4,10 @@ require './contact'
 require	'./rolodex'
 
 
-$rolodex= Rolodex.new
+$rolodex = Rolodex.new
+contact = $rolodex.find(1000)
+	$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
+
 
 get '/' do
 	@crm_name = "Dan Weinstock's CRM"
@@ -20,6 +23,10 @@ get '/contacts/new' do
 	erb :new
 end
 
+get "/contacts/1000" do
+  @contact = @@rolodex.find(1000)
+  erb :show_contact
+end
 post '/contacts' do
 	contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	$rolodex.add_contact(contact)
--- rolodex.rb
+++ rolodex.rb
@@ -4,48 +4,44 @@ class Rolodex
 	def initialize
 		@contacts = []
 		@id = 1000
+		
 	end
-
+	
 	def add_contact(contact)
 		contact.id = @id
 		@contacts << contact
 		@id += 1
 	end
+
+
+	def modify_a_contact(contact)
+
+		puts "\nChoose an attribute to modify: \n[1] First Name \n[2] Last Name \n[3] Email \n[4] Note"
+		answer = gets.chomp.to_i
+
+		case answer
+		when 1 
+			puts "Enter a modified First Name"
+			firstname = gets.chomp.capitalize
+			contact.first_name = firstname
+		when 2
+			puts "Enter a modified Last Name"
+			lastname = gets.chomp.capitalize
+			contact.last_name = lastname
+		when 3 
+			puts "Enter a modified Email Address"
+			emailaddress = gets.chomp
+			contact.email = emailaddress
+		when 4 
+			puts "Enter a modified note"
+			newnote = gets.chomp
+			contact.note = newnote
+		end
+		
+	end
+
+	def find(contact_id)
+    @contacts.find {|contact| contact.id == contact_id }
+  end
 end
 
-def modify_contact(id)
-		modified_contact = display_contact(id)
-		
-		puts '[1] Modify First Name' 
-		puts '[2] Modify Last Name' 
-		puts '[3] Modify Email'
-		puts '[4] Modify Notes'
-		puts 'Enter a number: '
-		
-		modify_number = gets.chomp.to_i
-
-		case modify_number
-			when 1
-				print "New First Name: "
-				new_first_name = gets.chomp
-				# puts "You have changed first name to: #{new_first_name}"
-				modified_contact.first_name = new_first_name
-			when 2 
-				print "New Last Name: "
-				new_last_name = gets.chomp
-				# puts "You have changed last name to: #{new_last_name}"
-				modified_contact.last_name = new_last_name
-			when 3
-				print "New Email: "
-				new_email = gets.chomp
-				# puts "You have changed email to: #{new_email}"
-				modified_contact.email = new_email
-			when 4
-				print "New Note: "
-				new_note = gets.chomp
-				# puts "You have changed note to: #{new_note}"
-				modified_contact.note = new_note
-			else 
-				puts "That is not a valid response"
-		end 
-	end	
