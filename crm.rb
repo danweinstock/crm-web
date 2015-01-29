7f2612ef8f5260bfb308a8390ab29a3d5d232996
danweinstock <dan.weinstock@yahoo.com>
2015-01-29 17:51:34 -0500

    part 3 complete

--- crm.rb
+++ crm.rb
@@ -32,8 +32,45 @@ get "/contacts/:id" do
 		end
 end
 
+get "/contacts/:id/edit" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 post '/contacts' do
 	contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	$rolodex.add_contact(contact)
 	redirect to('/contacts')
 end
+
+put "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+    
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete "/contacts/:id" do
+  @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    $rolodex.remove_contact(@contact)
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+
+
+
